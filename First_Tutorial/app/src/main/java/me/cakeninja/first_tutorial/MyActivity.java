8a5db7596691ba06ec944470e3a9f9bad3acba9f
Jorge Perez <jorslu@gmail.com>
2015-03-02 23:53:42 -0500

    Finished the 'Start Another Activity' sub section. Completed the First Tutorial 'Building Your First App'

--- First_Tutorial/app/src/main/java/me/cakeninja/first_tutorial/MyActivity.java
+++ First_Tutorial/app/src/main/java/me/cakeninja/first_tutorial/MyActivity.java
@@ -1,19 +1,23 @@
 package me.cakeninja.first_tutorial;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "me.cakeninja.first_tutorial.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +39,19 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when the user clicks the Send button
+     *
+     * @param view
+     */
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
